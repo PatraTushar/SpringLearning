cf75a9dbc2435ec6bede946d3f3435cd50125536
Tushar Patra <tushar04012002@gmail.com>
2026-06-09 00:35:57 +0530

    dsa

---
+++ src/main/java/org/Example7/MainApp.java
@@ -0,0 +1,27 @@
+package org.Example7;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class MainApp {
+
+    public static void main(String[] args) {
+
+        ApplicationContext container = new ClassPathXmlApplicationContext("bean7.xml");
+
+        patient p1 = (patient) container.getBean("patient1");
+        System.out.println(p1);
+
+        patient p2 = (patient) container.getBean("patient2");
+        System.out.println(p2);
+
+        patient p3 = (patient) container.getBean("patient3");
+        System.out.println(p3);
+
+        patient p4 = (patient) container.getBean("patient4");
+        System.out.println(p4);
+
+
+
+    }
+}
---
+++ src/main/java/org/Example7/patient.java
@@ -0,0 +1,38 @@
+package org.Example7;
+
+public class patient {
+
+    private int patientID;
+    private String patientName;
+    private long mobileNumber;
+
+
+    public patient() {
+
+        System.out.println(" This is Default Constructor");
+    }
+
+    public patient(int patientID, String patientName, long mobileNumber) {
+
+        System.out.println(" The constructor has 3 parameters ");
+        this.patientID = patientID;
+        this.patientName = patientName;
+        this.mobileNumber = mobileNumber;
+    }
+
+    public patient(int patientID, String patientName) {
+
+        System.out.println(" The constructor has 2 parameters ");
+        this.patientID = patientID;
+        this.patientName = patientName;
+
+    }
+
+
+    public String toString() {
+
+        return "{" + patientID + "," + patientName + "," + mobileNumber + "}";
+    }
+
+
+}
